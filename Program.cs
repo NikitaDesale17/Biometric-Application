0dda8129efa1a4a8a841a14dbc64dadf6c53ab44
Nikita Desale <86356749+NikitaDesale17@users.noreply.github.com>
2025-04-22 19:23:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MxFace.Fingerprint.SDK.Sample.Net.Components;
+using MxFace.Fingerprint.SDK.Sample.Net.Data.Contexts;
+using MxFace.Fingerprint.SDK.Sample.Net.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.AddApplicationServices();
+
+//builder.AddApplicationServices();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
